084a7001cd8b227f3267f95869002b537cb66a8b
Michael Heinrichs <netopyr@users.noreply.github.com>
2020-09-27 08:54:44 +0200

    Initial project generated with Spring Initializr

---
+++ src/test/java/ch/fhnw/webec/converter/ConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.fhnw.webec.converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
